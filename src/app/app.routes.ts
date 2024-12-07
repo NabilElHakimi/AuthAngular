54bd4af4a3e50fd88ac702c0b7c62cf56e1cc4fa
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-12-07 17:29:25 +0100

    refactor: relocate AuthGuard to root directory and update import paths; remove old guard file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { HomePageComponent } from './home/home-page/home-page.component';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { AuthGuard } from './auth/auth.guard';
+import { AuthGuard } from '../auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
